e3d2befe21e8cd8ad46afa3c800cb13c80c55fa5
shahbajjamil <shahbajjamil2014@gmail.com>
2020-04-28 22:40:08 +0530

    Eid Mubarak 2020

---
+++ script/script.js
@@ -0,0 +1,5 @@
+    window.onload = function() {
+        var loader= document.querySelector(".preloader");
+        loader.className +=" active";
+        document.getElementById("eidid").play();
+    }
