b5c589dbf4ac2a4363687900b0e108440f558f1a
shahbajjamil <shahbajjamil2014@gmail.com>
2020-04-29 12:23:52 +0530

    audio on tap

--- script/script.js
+++ script/script.js
@@ -1,5 +1,11 @@
     window.onload = function() {
         var loader= document.querySelector(".preloader");
         loader.className +=" active";
+        playeid();
+    }
+    window.onclick = function() {
+        playeid();
+    }
+    function playeid() {
         document.getElementById("eidid").play();
     }
